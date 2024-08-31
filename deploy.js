5d6bdec99bdab87972e76f0fa7ba1d4b2916e3e8
khoint <khoint@khoints-MacBook-Air.local>
2024-09-01 00:27:37 +0700

    Update to lastest version

--- deploy.js
+++ deploy.js
@@ -1,27 +1,26 @@
-// 311e8ca40c9a4e3ea42b8be40bfafb69
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
-const { interface, bytecode } = require("./compile");
 
+const { abi, evm } = require('./compile');
 
-const provider = new HDWalletProvider(
-    "grit copper thank burden panther hood work border animal outer prosper stand",
-    // remember to change this to your own phrase!
-    "https://sepolia.infura.io/v3/311e8ca40c9a4e3ea42b8be40bfafb69"
-    // remember to change this to your own endpoint!
-  );
-  const web3 = new Web3(provider);
+provider = new HDWalletProvider(
+  "grit copper thank burden panther hood work border animal outer prosper stand",
+  "https://sepolia.infura.io/v3/311e8ca40c9a4e3ea42b8be40bfafb69"
+);
 
-  const deploy = async () => {
-    const accounts = await web3.eth.getAccounts();
+const web3 = new Web3(provider);
 
-    console.log("Attempting to deploy from account", accounts[0]);
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
 
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-      .deploy({ data: bytecode, arguments: ["Hi there!"] })
-      .send({ gas: "1000000", from: accounts[0] });
+  console.log('Attempting to deploy from account', accounts[0]);
 
-    console.log("Contract deployed to", result.options.address);
-    provider.engine.stop();
-  };
-  deploy();
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
